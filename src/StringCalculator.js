d3af5c90fa69afc3f7aadd0f667e68dbc1efc28c
Jignesh Kukreja <jigneshkukreja@gmail.com>
2024-12-02 16:49:28 +0530

    checked if number start with double slash & fixed solution for 6th test case

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -2,7 +2,13 @@ export const add = (numbers) => {
     if (!numbers) {
         return 0;
     }
-    numbers = numbers.replace(/\n/g, ",");
+    let delimiter = ",";
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = parts[0].substring(2);
+      numbers = parts[1];
+    }
+    numbers = numbers.replace(/\n/g, delimiter);
     const parts = numbers.split(",").map(Number);
     return parts.reduce((sum, num) => sum + num, 0);
 };
