659ec252d683b31203d3fc2b10fa9448707e537a
Jignesh Kukreja <jigneshkukreja@gmail.com>
2024-12-02 16:23:10 +0530

    wrote test case for single number

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -3,3 +3,8 @@ import { add } from './StringCalculator';
 test('should return 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('should return the number for a single number input', () => {
+    expect(add("1")).toBe(1);
+  });
+  
