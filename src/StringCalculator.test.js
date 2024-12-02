2b48fece7b29d0566d35de5ac921e99bbf6a546b
Jignesh Kukreja <jigneshkukreja@gmail.com>
2024-12-02 16:58:44 +0530

    test case for negative number changed with list of negative numbers

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -24,6 +24,6 @@ test('should support custom delimiters', () => {
     expect(add("//;\n1;2")).toBe(3);
 });
 
-test('should throw an exception for negative numbers', () => {
-    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
-});
+test('should list all negative numbers in the exception message', () => {
+    expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed: -2, -3");
+  });
