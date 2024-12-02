dcb7ff237914a43e6b7c0e27744bb066259352c5
Jignesh Kukreja <jigneshkukreja@gmail.com>
2024-12-02 16:37:03 +0530

    replaced new line delimiter with coma and handled it for 5th test case

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -2,6 +2,7 @@ export const add = (numbers) => {
     if (!numbers) {
         return 0;
     }
+    numbers = numbers.replace(/\n/g, ",");
     const parts = numbers.split(",").map(Number);
     return parts.reduce((sum, num) => sum + num, 0);
 };
