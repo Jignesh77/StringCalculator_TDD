2efa19a76e6eed9c7ae8947eed07ced4375f5043
Jignesh Kukreja <jigneshkukreja@gmail.com>
2024-12-02 16:27:01 +0530

    wrote test case for handling 3rd condition

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -1,10 +1,13 @@
 import { add } from './StringCalculator';
 
 test('should return 0 for an empty string', () => {
-  expect(add("")).toBe(0);
+    expect(add("")).toBe(0);
 });
 
 test('should return the number for a single number input', () => {
     expect(add("1")).toBe(1);
-  });
-  
+});
+
+test('should return the sum for two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
